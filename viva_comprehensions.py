3194964b75be8a266cc461374e6295a6622f8db5
Tim L <timl@Zipcoders-MacBook-Pro.local>
2024-07-16 17:09:19 -0400

    first two

--- viva_comprehensions.py
+++ viva_comprehensions.py
@@ -8,32 +8,48 @@ class Parity(enum.Enum):
 
 
 def gen_list(start: int, stop: int, parity: Parity) -> List[int]:
-    """
+
+   """
+    generate a list of integers from start to stop that match the parity condition
+
     Oh no some evil developer decided not to write docstrings. Maybe you can use the test cases to decipher
     what this method was supposed to do. Hey if you do, maybe you could do some good in this world by
     updating this here docstring to something useful.
 
-    :param start:
-    :param stop:
-    :param parity:
-    :return:
+    :param start: starting integer
+    :param stop: ending integer
+    :param parity: parity condition match (odd or even)
+    :return: a list of integers matching the parity condition
     """
-    pass
+   
+   if parity == Parity.ODD:
+      return [i for i in range(start, stop) if i % 2 != 0] # return odd numbers
+   elif parity == Parity.EVEN:
+      return [i for i in range(start, stop) if i % 2 == 0] # return even numbers
+    #pass
 
 
 def gen_dict(start: int, stop: int, strategy: Callable) -> Dict:
+
     """
+    create a dictionary where the keys are the numbers from start to stop (inclusive) and the values are
+    the squared values of the keys
+
+    
+
     Oh no some evil developer decided not to write docstrings. Maybe you can use the test cases to decipher
     what this method was supposed to do. Hey if you do, maybe you could do some good in this world by
     updating this here docstring to something useful.
 
 
-    :param start:
-    :param stop:
-    :param strategy:
-    :return:
+    :param start: starting integer
+    :param stop: ending integer
+    :param strategy: stragegy, which is a callable object (function or lambda)
+    :return: return a dictionary where keys are the numbers from start to stop and the values are based on the strategy function
     """
-    pass
+    return {i: strategy(i) for i in range(start, stop)} 
+    #constructs a dictionary with i as the key and strategy(i) as the value
+    #pass
 
 
 def gen_set(val_in: str) -> Set:
@@ -42,7 +58,9 @@ def gen_set(val_in: str) -> Set:
     what this method was supposed to do. Hey if you do, maybe you could do some good in this world by
     updating this here docstring to something useful.
 
-    :param val_in:
-    :return:
+    :param val_in: input string
+    :return: A set of unique characters (lowercase) from the input string.
     """
-    pass
+    if char for char in val_in if char.isalpha()}  # filters out non-alphabetic characters
+    #return {char for char in val_in.lower }  #returns set where characters are converted to lowercase in the input string
+    #pass
